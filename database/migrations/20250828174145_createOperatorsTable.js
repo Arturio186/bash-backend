d35d353ab20e449c3a14279181627fd4148b61be
Ernest Fisher <e.fisher@frmndev.ru>
2025-08-28 23:36:34 +0500

    feat: change migrations

---
+++ database/migrations/20250828174145_createOperatorsTable.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("operators", (table) => {
+    table.increments("id").primary();            
+    table.string("tg_username").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("operators");
+};
